7588e09d7a4273c72acd04f8ba50e333ae6ca616
rithvick <munagalarithvickreddy@gmail.com>
2024-02-10 19:19:57 -0500

    Completed Instructions

--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
-const express = require('express');
+// Budget API
 const fs = require('fs');
+const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-
-app.use('/', express.static('public'));
 
 app.use(cors());
-var budget;
+
+
 fs.readFile('data.json', 'utf8', (err, file) => {
-    budget = file;
+    data = file;
     return;
 })
 
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/budget', (req, res) => {
-    res.send(budget);
+    res.send(data);
 });
+
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
